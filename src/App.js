1f70cd8989530d5e249586525b0c0dc7420f0b84
Gabcii <gderzsi@yahoo.co.uk>
2026-08-21 12:21:38 +0100

    added boostrap

--- src/App.js
+++ src/App.js
@@ -5,31 +5,33 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      {" "}
-      Weather App;
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/Gabcii" target="_blank" rel="noreferrer">
-          Gabriella Derzsi{" "}
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://github.com/Gabcii/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{" "}
-        and
-        <a
-          href="https://react-weather-app-gd.netlify.app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-        .
-      </footer>
+      <div className="container">
+        {" "}
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/Gabcii" target="_blank" rel="noreferrer">
+            Gabriella Derzsi{" "}
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://github.com/Gabcii/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and
+          <a
+            href="https://react-weather-app-gd.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
