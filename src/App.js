65246384f46c4c8940f2d9dd09aaaf9e16c3a0e4
Gabcii <gderzsi@yahoo.co.uk>
2026-08-21 12:08:24 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -3,5 +3,33 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App</div>;
+  return (
+    <div className="App">
+      {" "}
+      Weather App;
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Gabcii" target="_blank" rel="noreferrer">
+          Gabriella Derzsi{" "}
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://github.com/Gabcii/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and
+        <a
+          href="https://react-weather-app-gd.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
